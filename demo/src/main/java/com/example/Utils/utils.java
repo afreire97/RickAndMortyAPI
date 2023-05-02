c58fa39133560b86553f9b0a3a95553d1a6ac8bb
Aaron F <freiresousa97@gmail.com>
2023-05-02 08:46:26 +0200

    Utils

--- demo/src/main/java/com/example/Utils/utils.java
+++ demo/src/main/java/com/example/Utils/utils.java
@@ -23,11 +23,10 @@ public static int getRandom(int max){
 }
 public static String getPage(String url){
 
-    String [] trozos = url.split("/");
+    String [] trozos = url.split("=");
 
 
-String num = trozos[trozos.length-1];
-return num;
+return = trozos[trozos.length-1];
 
 }
 }
