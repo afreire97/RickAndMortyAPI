e793eaa5029e7e297bb0bc91ce0fe7ab4472cbe8
Aaron F <freiresousa97@gmail.com>
2023-05-02 12:54:09 +0200

    nav10

--- demo/src/main/java/com/example/demo/controller/WebController.java
+++ demo/src/main/java/com/example/demo/controller/WebController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,8 @@ import com.example.demo.models.CharactersModel;
 import com.example.demo.service.RickAndMortyService;
 
 @Controller
+@CrossOrigin(origins = "https://rickandmortyapi-production.up.railway.app")
+
 public class WebController {
 
     @Autowired
